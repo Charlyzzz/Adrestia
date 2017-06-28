c8d3ef865a6a1a8ff18e6fcbfb9f4d98bb9a9124
Federico Scarpa <fedescarpa@gmail.com>
2017-06-28 18:51:32 -0300

    changing supervision tree

--- lib/adrestia/active_check.ex
+++ lib/adrestia/active_check.ex
@@ -1,13 +1,13 @@
 defmodule Adrestia.ActiveCheck do
 
-  def health_check(endpoints, strategy) do
+  def health_check(endpoints) do
     Enum.each(endpoints,fn(endpoint) ->
-      GenServer.cast(strategy, {server_status(endpoint), endpoint})
+      GenServer.cast(Adrestia.Balancer, {server_status(endpoint), endpoint})
     end)
-    GenServer.cast(strategy, :status)
+    GenServer.cast(Adrestia.Balancer, :status)
   end
 
-  defp server_status({_, url}) do
+  defp server_status(%{:host => url}) do
     case HTTPotion.get(url) do
         %HTTPotion.Response{} -> :server_up
         %HTTPotion.ErrorResponse{} -> :server_down
