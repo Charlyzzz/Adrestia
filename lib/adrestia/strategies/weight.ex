0f4691337667412582efedb073ace18d33aeb30e
Federico Scarpa <fedescarpa@gmail.com>
2017-06-28 21:02:04 -0300

    delegating som ugly code

--- lib/adrestia/strategies/weight.ex
+++ lib/adrestia/strategies/weight.ex
@@ -1,16 +1,30 @@
 defmodule Adrestia.Weight do
 
   def next_server(endpoints) do
-    [server|rest] = Enum.sort_by(endpoints, &Map.get(&1, :remaining_weight), &>=/2)
+    [server|rest] = sort_servers_by_remaining_weight(endpoints)
 
-    {_, next_server} = Map.get_and_update(server, :remaining_weight, fn(prev_weight) -> {prev_weight, prev_weight - 1} end)
+    {_, next_server} = update_remaining_weight(server)
 
-    new_endpoints = [next_server|rest]
-    if (Enum.all?(new_endpoints, fn(%{:remaining_weight => value}) -> value == 0 end)) do
-      new_endpoints = Enum.map(new_endpoints, fn(%{:weight => weight} = server) -> Map.put(server, :remaining_weight, weight) end)
-    end
-
-    {next_server, new_endpoints}
+    {next_server, get_endpoints_from([next_server|rest])}
   end
 
+  defp sort_servers_by_remaining_weight(endpoints) do
+    Enum.sort_by(endpoints, &Map.get(&1, :remaining_weight), &>=/2)
+  end
+
+  defp update_remaining_weight(server) do
+    Map.get_and_update(server, :remaining_weight, fn(prev_weight) -> {prev_weight, prev_weight - 1} end)
+  end
+
+  defp get_endpoints_from(endpoints) do
+    if all_remaining_weight_zero?(endpoints) do
+      Enum.map(endpoints, fn(%{:weight => weight} = server) -> Map.put(server, :remaining_weight, weight) end)
+    else
+      endpoints
+    end
+  end
+
+  defp all_remaining_weight_zero?(endpoints) do
+    Enum.all?(endpoints, fn(%{:remaining_weight => value}) -> value == 0 end)
+  end
 end
