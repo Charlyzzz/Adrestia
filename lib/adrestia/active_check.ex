28abaf1252e9c89a00f72e46a57d153c4ab9fbb9
Charlyzzz <erwincdl@gmail.com>
2017-07-04 00:49:15 -0300

    check pattern matching

--- lib/adrestia/active_check.ex
+++ lib/adrestia/active_check.ex
@@ -1,14 +1,13 @@
 defmodule Adrestia.ActiveCheck do
 
-  def health_check do
-    endpoints = GenServer.call(Adrestia.Balancer, :endpoints)
-    Enum.each(endpoints, fn(endpoint) ->
-      GenServer.cast(Adrestia.Balancer, {server_status(endpoint), endpoint})
+  def health_check(endpoints) do
+    Enum.each(endpoints, fn(%{:host => host}) ->
+      GenServer.cast(Adrestia.Balancer, {server_status(host), host})
     end)
     GenServer.cast(Adrestia.Balancer, :status)
   end
 
-  defp server_status(%{:host => url}) do
+  defp server_status(url) do
     case HTTPotion.head(url) do
         %HTTPotion.Response{} -> :server_up
         %HTTPotion.ErrorResponse{} -> :server_down
